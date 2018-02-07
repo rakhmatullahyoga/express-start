c1eac22af9003d802f6c962e4f3491619caef5af
rakhmatullahyoga <rakhmatullahyoga@gmail.com>
2018-02-07 11:03:32 +0700

    update init-db script

--- build/init-db.sh
+++ build/init-db.sh
@@ -2,8 +2,7 @@
 echo "============================================"
 echo "Run database migration and seeds . . . ."
 echo "============================================"
-docker-compose run --rm api node node_modules/.bin/sequelize db:migrate
-docker-compose run --rm api node node_modules/.bin/sequelize db:seed:all
+docker-compose run --rm api /bin/bash -c "node node_modules/.bin/sequelize db:migrate && node node_modules/.bin/sequelize db:seed:all"
 echo "============================================"
 echo "Database initialization succeed!"
 echo "============================================"
